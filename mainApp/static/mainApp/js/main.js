9fcf6be36e6974d0edc07f7e14a323aeee518ccb
Alejandro Martinez <alejomart789@gmail.com>
2023-04-09 18:45:50 -0500

    Commit actualizacion del proyecto desde 0 y creacion del nav de header

---
+++ mainApp/static/mainApp/js/main.js
@@ -0,0 +1,7 @@
+/* Código JavaScript para la página */
+
+/* Función que se ejecuta cuando se carga la página */
+$(document).ready(function() {
+    console.log("La página ha cargado.");
+  });
+  
