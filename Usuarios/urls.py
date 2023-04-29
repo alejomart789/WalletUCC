9ce9b9e22647b6613e78424504577968aeac16ed
Alejandro Martinez <alejomart789@gmail.com>
2023-04-29 11:57:02 -0500

    Se configuro el boton de salr del dropdon del nombre de usuario en consola estudiantes

--- Usuarios/urls.py
+++ Usuarios/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+from django.urls import path, include, reverse_lazy
 import Usuarios.views
+from django.contrib.auth.views import LogoutView
+
 
 urlpatterns = [
     path('login/', Usuarios.views.login_user, name='login_user'),
     path('consola_estudiantes/', Usuarios.views.consola_estudiantes, name='consola_estudiantes'),
+    
+    path('mainApp/', include('mainApp.urls', namespace='main_app')),
+    path('logout/', LogoutView.as_view(next_page=reverse_lazy('main_app:inicio')), name='logout'),
 ]
