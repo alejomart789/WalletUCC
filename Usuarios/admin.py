410c35fdd4e94f821f5749c081d43b6eee171302
Alejandro Martinez <alejomart789@gmail.com>
2023-05-01 00:37:37 -0500

    Se termino la base de datos de modelos

--- Usuarios/admin.py
+++ Usuarios/admin.py
@@ -1,15 +1,16 @@
 from django.contrib import admin
-from Usuarios.models import Usuario, Cuenta, Dependencia, Envio, Transaccion
+from Usuarios.models import Usuario, Cuenta, Dependencia, Envio, Transaccion, Estudiante
 
 # Register your models here.
 admin.site.register(Usuario)
+admin.site.register(Estudiante)
 admin.site.register(Cuenta)
 admin.site.register(Envio)
 admin.site.register(Transaccion)
 
 @admin.register(Dependencia)
 class DependenciaAdmin(admin.ModelAdmin):
-    list_display = ('id_dependencia', 'nombre', 'tipo_dependencia', 'saldo', 'ultima_actualizacion')
+    list_display = ('id_dependencia', 'nombre_dependencia', 'tipo_dependencia', 'saldo_dependencia', 'ultima_actualizacion_dependencia')
     list_filter = ('tipo_dependencia',)
-    search_fields = ('nombre',)
+    search_fields = ('nombre_dependencia',)
 
--- Usuarios/models.py
+++ Usuarios/models.py
@@ -2,18 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 from django.utils import timezone
-
-ROL_USUARIO_CHOICES = [('Estudiante', 'Estudiante'), ('Profesor', 'Profesor'), ('Dependencia', 'Dependencia')]
+from django.core.exceptions import PermissionDenied
 
 class Usuario(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='usuario')
-    nombres = models.CharField(max_length=50)
-    apellidos = models.CharField(max_length=50)
-    rol = models.CharField(max_length=50, choices=ROL_USUARIO_CHOICES)
-    identificacion = models.PositiveIntegerField(unique=True)
-    email = models.EmailField()
-    foto_perfil = models.ImageField(upload_to='Usuarios/img/Perfil', default='Usuarios/img/Perfil/default.png', blank=True)
-    amigos = models.ManyToManyField('self', blank=True)
+    nombres_usuario = models.CharField(max_length=50)
+    apellidos_usuario = models.CharField(max_length=50)
+    identificacion_usuario = models.PositiveIntegerField(unique=True)
+    email_usuario = models.EmailField()
+    foto_perfil_usuario = models.ImageField(upload_to='Usuarios/img/Perfil', default='Usuarios/img/Perfil/default.png', blank=True)
+    amigos_usuario = models.ManyToManyField('self', blank=True)
 
     def __str__(self):
         return self.user.username
@@ -22,19 +20,19 @@ class Usuario(models.Model):
         verbose_name = "Usuario"
         verbose_name_plural = "Usuarios"
 
-    def clean(self):
-        if self.rol == 'Estudiante' and hasattr(self, 'profesor'):
-            raise ValidationError('Un usuario no puede ser estudiante y profesor a la vez')
-        if self.rol == 'Profesor' and hasattr(self, 'estudiante'):
-            raise ValidationError('Un usuario no puede ser estudiante y profesor a la vez')
+class Estudiante(models.Model):
+    usuario_estudiante = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    valor_semestre_estudiante = models.DecimalField(max_digits=15, decimal_places=2)
+    creditos_registrados_estudiante = models.PositiveIntegerField()
+    semestre_actual_estudiante = models.PositiveIntegerField()
 
 
 class Cuenta(models.Model):
-    usuario = models.OneToOneField(Usuario, on_delete=models.CASCADE, related_name='cuenta')
-    saldo = models.DecimalField(max_digits=10, decimal_places=2)
+    usuario_cuenta = models.OneToOneField(Usuario, on_delete=models.CASCADE, related_name='Cuenta')
+    saldo_usuario = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
-        return self.usuario.user.username
+        return self.usuario_cuenta.user.username
 
     class Meta:
         verbose_name = "Cuenta"
@@ -44,14 +42,14 @@ class Cuenta(models.Model):
 class Dependencia(models.Model):
     ROL_DEPENDENCIA_CHOICES = [('Financiera', 'Financiera'), ('Otra', 'Otra')]
     id_dependencia = models.PositiveIntegerField(unique=True)
-    nombre = models.CharField(max_length=50)
+    nombre_dependencia = models.CharField(max_length=50)
     tipo_dependencia = models.CharField(max_length=50, choices=ROL_DEPENDENCIA_CHOICES)
-    saldo = models.DecimalField(max_digits=10, decimal_places=2)
-    usuarios = models.ManyToManyField(Usuario, related_name='dependencias', blank=True)
-    ultima_actualizacion = models.DateTimeField(auto_now=True)
+    saldo_dependencia = models.DecimalField(max_digits=10, decimal_places=2)
+    usuarios_dependencia = models.ManyToManyField(Usuario, related_name='dependencias', blank=True)
+    ultima_actualizacion_dependencia = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.nombre
+        return self.nombre_dependencia
 
     class Meta:
         verbose_name = "Dependencia"
@@ -60,11 +58,11 @@ class Dependencia(models.Model):
 
 class Envio(models.Model):
     id_envio = models.PositiveIntegerField(unique=True)
-    origen = models.ForeignKey(Cuenta, on_delete=models.CASCADE, related_name='envios_realizados')
-    destino = models.ForeignKey(Cuenta, on_delete=models.CASCADE, related_name='envios_recibidos')
-    fecha = models.DateTimeField(auto_now_add=True)
-    monto = models.DecimalField(max_digits=10, decimal_places=2)
-    estado = models.BooleanField(default=False)
+    origen_cuenta = models.ForeignKey(Cuenta, on_delete=models.CASCADE, related_name='envios_realizados')
+    destino_cuenta = models.ForeignKey(Cuenta, on_delete=models.CASCADE, related_name='envios_recibidos')
+    fecha_envio = models.DateTimeField(auto_now_add=True)
+    monto_envio = models.DecimalField(max_digits=10, decimal_places=2)
+    estado_envio = models.BooleanField(default=False)
 
     def __str__(self):
         return f'Envío {self.id_envio} de {self.origen.usuario.nombres} {self.origen.usuario.apellidos} a {self.destino.usuario.nombres} {self.destino.usuario.apellidos}'
@@ -75,38 +73,38 @@ class Envio(models.Model):
 
 
 class Transaccion(models.Model):
-    dependencia = models.ForeignKey(Dependencia, on_delete=models.CASCADE, related_name='transacciones')
-    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='transacciones')
-    info_factura = models.CharField(max_length=100)
-    monto = models.DecimalField(max_digits=10, decimal_places=2)
-    fecha = models.DateTimeField(auto_now_add=True)
-    fecha_vencimiento = models.DateTimeField(null=True, blank=True)
-    pagada = models.BooleanField(default=False)
+    dependencia_origien = models.ForeignKey(Dependencia, on_delete=models.CASCADE, related_name='transacciones')
+    usuario_destino = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='transacciones')
+    info_factura_dependencia = models.CharField(max_length=100)
+    monto_transaccion = models.DecimalField(max_digits=10, decimal_places=2)
+    fecha_transaccion = models.DateTimeField(auto_now_add=True)
+    fecha_vencimiento_transaccion = models.DateTimeField(null=True, blank=True)
+    pagada_transaccion = models.BooleanField(default=False)
 
     def __str__(self):
-        return f'Transacción {self.id} de {self.usuario.user.username} a {self.dependencia.nombre}'
+        return f'Transacción {self.id} de {self.usuario_destino.user.username} a {self.dependencia_origien.nombre_dependencia}'
 
     class Meta:
         verbose_name = "Transacción"
         verbose_name_plural = "Transacciones"
-        ordering = ['-fecha']
+        ordering = ['-fecha_transaccion']
 
     def clean(self):
         if self.monto <= 0:
             raise ValidationError('El monto de la transacción debe ser mayor a cero')
-        if self.fecha_vencimiento and self.fecha_vencimiento <= self.fecha:
+        if self.fecha_vencimiento_transaccion and self.fecha_vencimiento_transaccion <= self.fecha:
             raise ValidationError('La fecha de vencimiento debe ser posterior a la fecha actual')
     
     def save(self, *args, **kwargs):
         # Verificar que el usuario tenga suficiente saldo para pagar la transacción
-        if self.usuario.cuenta.saldo < self.monto:
+        if self.usuario_destino.Cuenta.saldo_usuario < self.monto_transaccion:
             raise ValidationError('El usuario no tiene suficiente saldo para realizar esta transacción')
         # Restar el monto de la cuenta del usuario
-        self.usuario.cuenta.saldo -= self.monto
-        self.usuario.cuenta.save()
+        self.usuario_destino.Cuenta.saldo_usuario -= self.monto_transaccion
+        self.usuario.Cuenta.save()
         # Añadir el monto a la cuenta de la dependencia
-        self.dependencia.saldo += self.monto
-        self.dependencia.save()
+        self.dependencia_origien.saldo_dependencia += self.monto_transaccion
+        self.Dependencia.save()
         super(Transaccion, self).save(*args, **kwargs)
 
     def actualizar_estado(self):
@@ -115,7 +113,7 @@ class Transaccion(models.Model):
             self.save()
 
     def notificar_usuario(self):
-        if not self.pagada and self.fecha_vencimiento and self.fecha_vencimiento <= timezone.now():
+        if not self.pagada_transaccion and self.fecha_vencimiento and self.fecha_vencimiento <= timezone.now():
             mensaje = f'La transacción {self.id} con la dependencia {self.dependencia.nombre} está pendiente de pago'
             # Enviar notificación al usuario
             pass
